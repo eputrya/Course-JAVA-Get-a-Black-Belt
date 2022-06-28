f5813bc3f3c696445e36923e53a92fd0ca32c1f6
udj <garden8b103@gmail.com>
2022-06-28 22:55:23 +0300

    add Example_3

--- src/comparation/Example_2.java
+++ src/comparation/Example_2.java
@@ -1,4 +1,7 @@
 package comparation;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class Employee implements Comparable<Employee> {
     int id;
@@ -31,5 +34,22 @@ class Employee implements Comparable<Employee> {
             res = this.surname.compareTo(anotherEmp.surname);
         }
         return res;
+    }
+}
+
+
+public class Example_2 {
+    public static void main(String[] args) {
+        List<Employee> list = new ArrayList<>();
+        Employee emp1 = new Employee(100, "Zaur", "Tregulov", 12345);
+        Employee emp2 = new Employee(15, "Ivan", "Petrov", 6542);
+        Employee emp3 = new Employee(123, "Ivan", "Sidorov", 8542);
+
+        list.add(emp1);
+        list.add(emp2);
+        list.add(emp3);
+        System.out.println("Before sorting \n" + list);
+        Collections.sort(list, new SalaryComparator());
+        System.out.println("After sorting \n" + list);
     }
 }
