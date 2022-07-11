d5e9fcfb82796d10113bf1e19f1c6f93e7a27903
Putrya Evgeniy <user135@bulat.ru>
2022-07-11 16:23:47 +0300

    test

--- src/Collections/ListIteratorExample.java
+++ src/Collections/ListIteratorExample.java
@@ -7,9 +7,8 @@ public class ListIteratorExample {
     public static void main(String[] args) {
         String s = "madam";
         List<Character> list = new LinkedList<>();
-        for(char ch: s.toCharArray()) {
-            list.add(ch);
-        }
+        for(char ch: s.toCharArray()) list.add(ch);
+
         ListIterator<Character> iterator  = list.listIterator();
         ListIterator<Character> reverseIterator = list.listIterator(list.size());
         boolean isPalindrome = true;
