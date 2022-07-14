6e945d3c125ed37851f3f08984c4811dae1e432d
Putrya Evgeniy <user135@bulat.ru>
2022-07-14 09:38:13 +0300

    add vector and stack

---
+++ src/Collections/StackExample2.java
@@ -0,0 +1,18 @@
+package Collections;
+import java.util.Stack;
+
+public class StackExample2 {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("Zaur");
+        stack.push("Misha");
+        stack.push("Oleg");
+        stack.push("Katya");
+        System.out.println(stack.peek());
+        System.out.println(stack);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+            System.out.println(stack);
+        }
+    }
+}
